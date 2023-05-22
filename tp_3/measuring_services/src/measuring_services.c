1d5ea6487fa277d252b9c1eac4dc4a5c724bea04
Gonzalo Carreno <kronleuchter85@gmail.com>
2023-05-22 14:22:42 +0100

    test pasa inicializando los dos servicios

--- tp_3/measuring_services/src/measuring_services.c
+++ tp_3/measuring_services/src/measuring_services.c
@@ -1,7 +1,14 @@
 #include "measuring_services.h"
 #include "dht11_service.h"
+#include "bmp280_service.h"
 
 void measuring_services_init(void){
 
-     dht11_service_init();
+    //
+    // inicializacion de modulos
+    //
+    dht11_service_init();
+
+    bmp280_service_init();
+
 }
