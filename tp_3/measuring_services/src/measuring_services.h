9c77c3205f7273aff45e39e0791ab201706bfac4
Gonzalo Carreno <kronleuchter85@gmail.com>
2023-05-22 14:05:28 +0100

    comenzando el desarrollo del servicio de mediciones usando TDD y la suite de testing vista en clase

---
+++ tp_3/measuring_services/src/measuring_services.h
@@ -0,0 +1,11 @@
+
+#ifndef MEASURING_SERVICE_H
+#define MEASURING_SERVICE_H
+
+
+#include "stdint.h"
+#include "stdbool.h"
+
+
+
+#endif
---
+++ tp_3/measuring_services/test/test_measuring_services.c
@@ -0,0 +1,12 @@
+#include "unity.h"
+
+#include "measuring_services.h"
+
+
+void test_measuring_services_init(){
+
+    //
+    // checkeamos que se inicializan los diferentes sub modulos
+    //
+   
+}
