fbe45a50bc05da920fa2d33e72a7c8f4e9051770
Gonzalo Carreno <kronleuchter85@gmail.com>
2023-05-22 14:07:50 +0100

    agregando el modulo dht11_service.h

--- tp_3/measuring_services/test/test_measuring_services.c
+++ tp_3/measuring_services/test/test_measuring_services.c
@@ -1,6 +1,7 @@
 #include "unity.h"
 
 #include "measuring_services.h"
+#include "mock_dht11_service.h"
 
 
 void test_measuring_services_init(){
@@ -8,5 +9,8 @@ void test_measuring_services_init(){
     //
     // checkeamos que se inicializan los diferentes sub modulos
     //
-   
+    
+    dht11_service_init_Expect();
+
+
 }
